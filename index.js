ccddcf5e62086bcc20fe18eab8a3f418c6db8210
Haris Ali <harisa73@gmail.com>
2021-08-04 20:38:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function myFunc()
+{
+    var nameValue = document.getElementById("form-search").value;
+    var display = "Hello to our version of Google.com Homepage, You have searched for: " + nameValue;
+    alert(display);
+}
